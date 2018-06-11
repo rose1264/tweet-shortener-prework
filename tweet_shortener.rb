cd5902da2094729dd7521f5dd34ead99827ae944
Shumei Huang <rose1264@gmail.com>
2018-06-11 12:04:45 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def word_substituter(string)
+  array = string.split(" ")
+  newArray = array.map do |word|
+    case word
+    when "hello"
+      word = "hi"
+    when "to" || "two" || "too"
+      word = "2"
+    when "for" || "four"
+      word = "4"
+    when "be"
+      word = "b"
+    when "you"
+      word = "u"
+    when "at"
+      word = "@"
+    when "and"
+      word = "&"
+    else
+      word
+    end
+  end
+  newArray.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    word_substituter(tweet)
+  end
+end
