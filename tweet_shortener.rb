0fd830ca0d00b09c52f435457b66754ba377c4d3
Shumei Huang <rose1264@gmail.com>
2018-06-11 12:22:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,8 +40,7 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
-  tweets.collect do |tweet|
+def selective_tweet_shortener(tweet)
     if tweet.length > 140
       word_substituter(tweet)
     else
